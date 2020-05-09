4c0769d9554c087cae07a5109ddcb25144663b91
PolishCodeGirl <malachowska.marzena@gmail.com>
2020-05-09 21:16:30 +0200

    Testing app with Jest

---
+++ __mocks__/@frontendmasters/pet.js
@@ -0,0 +1,34 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+import { act } from "@testing-library/react";
+
+const breeds = [
+    { name: 'Black' },
+    { name: 'White' },
+    { name: 'Red' },
+    { name: 'Blue' },
+    { name: 'Green' },
+];
+
+const doggos = JSON.parse(
+    readFileSync(path.join(__dirname, 'res.json')).toString()
+);
+
+export const ANIMALS = ['dog', 'cat', 'bird'];
+export const _breeds = breeds;
+export const _dogs = doggos.animals;
+
+const mock = {
+    breeds: jest.fn(() => {
+        return {
+            then: callback => act(() => callback({ breeds }))
+        }
+    }),
+    animals: jest.fn(() => {
+        return {
+            then: callback => act(() => callback(doggos))
+        }
+    })
+};
+
+export default mock;
